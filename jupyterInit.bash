99c410ca578fd295004d716be07187f20bc666d5
unset <unset>
2023-02-28 09:54:41 +0800

    adding bash script

---
+++ jupyterInit.bash
@@ -0,0 +1,14 @@
+#adding custom jupyter themes
+
+pip install jupyterthemes
+jt -l
+jt -t onedork
+
+#setting up git settings
+
+git config --global user.name "jason6356"
+git config --global user.email "xiaodemon6536@gmail.com"
+
+HTTP_CODE=$(curl --write-out "%{http_code}\n"
+
+
